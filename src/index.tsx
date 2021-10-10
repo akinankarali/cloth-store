65241f047f1056529c30f01266bfaf90b5b737b5
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-10-10 14:41:03 +0300

    feat: add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './view/App';
 import reportWebVitals from './reportWebVitals';
+import './style/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
