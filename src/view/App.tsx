b3f2beea65e5b2133058e747750dec04dc495b11
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-10-10 14:31:40 +0300

    refactor: update file structure

---
+++ src/view/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hellooooooo</p>
+    </div>
+  );
+}
+
+export default App;
