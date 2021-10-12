0d931c6980b86793ab92d239cc64589521fdfef4
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-10-12 14:32:14 +0300

    feat: add tailwindcss to storybook

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,13 +11,9 @@ export interface Props extends HTMLAttributes<HTMLButtonElement> {
 export const Button = ({children, variant='primary', ...props}: Props) => {
     return (
         <button 
+            className="text-white border-0 rounded-full p-2 cursor-pointer"
             style={{
                 backgroundColor: variant === 'primary' ? 'blue' : 'gray',
-                color: 'white',
-                border: 'none',
-                borderRadius: 100,
-                padding: 10,
-                cursor: 'pointer'
             }}
             type="button"
             {...props}>
