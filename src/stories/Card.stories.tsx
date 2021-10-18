4b6329c34db310661dabbb65922c1e42c2f8435c
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-10-18 09:49:30 +0300

    feat: add props for dynamic card

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -12,3 +12,15 @@ export default meta;
 const Templete: Story<Props> = (args) => <Card {...args}/>
 
 export const Default = Templete.bind({})
+
+
+Default.args = {
+    title: 'A Nice Dress',
+    description: 'This dress made by cotton and beautiful',
+    currency: '€',
+    price: 99.99,
+    score: 3.9,
+    category: 'clothes',
+    isInCart: false,
+    img: 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+}
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -17,18 +17,126 @@ function App() {
       </Switch>
       <div className="content container mx-auto pt-12">
         <div className="cards-wrapper flex flex-wrap -mx-4 justify-center">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card 
+            title="Deneme Title"
+            description= "Deneme Desc"
+            currency= '£'
+            price= {20.50}
+            score= {5}
+            category="Elektronic"
+            isInCart= {true}
+            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            />
+          <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
+          <Card 
+            title="Deneme Title"
+            description= "Deneme Desc"
+            currency= '$'
+            price= {1000}
+            score= {5}
+            category="Elektronic"
+            isInCart= {false}
+            img="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2071&q=80"
+            />
+          <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '$'
+            price= {1000}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2071&q=80'
+            />
+          <Card 
+            title="Deneme Title"
+            description= "Deneme Desc"
+            currency= '£'
+            price= {20.50}
+            score= {5}
+            category="Elektronic"
+            isInCart= {true}
+            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            />
+          <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
+          <Card 
+            title="Deneme Title"
+            description= "Deneme Desc"
+            currency= '£'
+            price= {20.50}
+            score= {5}
+            category="Elektronic"
+            isInCart= {false}
+            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            />
+          <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
+          <Card 
+            title="Deneme Title"
+            description= "Deneme Desc"
+            currency= '£'
+            price= {20.50}
+            score= {5}
+            category="Elektronic"
+            isInCart= {true}
+            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            />
+            <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
+            <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
+            <Card 
+            title='A Nice Dress'
+            description= 'This dress made by cotton and beautiful'
+            currency= '€'
+            price= {99.99}
+            score= {3.9}
+            category= 'Clothes'
+            isInCart= {false}
+            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+            />
         </div>
       </div>
     
