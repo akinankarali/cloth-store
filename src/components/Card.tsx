772bf9aabcb8d33e059c20e8f8cce71a556ba786
KARIYER\saim.ankarali <saim.ankarali@kariyer.net>
2021-10-18 12:55:02 +0300

    feat: make axios call then send props to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ export const Card = ({...props}: Props) => {
     return (
         <div className="card w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4 m-4 block bg-white shadow-lg hover:shadow-xl rounded-lg overflow-hidden">
             <div className="relative pb-96 overflow-hidden">
-                <img className="absolute inset-0 h-full w-full object-cover" src={props.img} alt="" />
+                <img className="absolute inset-0 h-full w-full object-contain" src={props.img} alt="" />
             </div>
             <div className="p-4 border-t border-b text-sm text-gray-700">
                 <h2 className="mt-2 mb-2 font-bold">{props.title}</h2>
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/app.css';
 import Header from '../components/Header'
 import Login from './Login'
 import SignUp from './Signup'
 import { Switch, Route } from 'react-router-dom'
 import Card from '../components/Card'
+import { AxiosResponse } from 'axios'
+import { IProductsData } from "../types/products.type"
+import ProductsDataService from '../services/products.service'
+
 
 
 function App() {
+  const [products, setProducts] = useState<IProductsData[]>([]);
+  
+  useEffect(() => {
+    ProductsDataService.getAll().then((response: AxiosResponse<IProductsData[]>) => {
+      setProducts(response.data)
+    })
+  }, []);
+
   return (
     <div className="App">
       <Switch>
@@ -17,126 +29,19 @@ function App() {
       </Switch>
       <div className="content container mx-auto pt-12">
         <div className="cards-wrapper flex flex-wrap -mx-4 justify-center">
-          <Card 
-            title="Deneme Title"
-            description= "Deneme Desc"
-            currency= '£'
-            price= {20.50}
-            score= {5}
-            category="Elektronic"
-            isInCart= {true}
-            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-            />
-          <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
-            />
-          <Card 
-            title="Deneme Title"
-            description= "Deneme Desc"
-            currency= '$'
-            price= {1000}
-            score= {5}
-            category="Elektronic"
-            isInCart= {false}
-            img="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2071&q=80"
-            />
-          <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '$'
-            price= {1000}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2071&q=80'
-            />
-          <Card 
-            title="Deneme Title"
-            description= "Deneme Desc"
-            currency= '£'
-            price= {20.50}
-            score= {5}
-            category="Elektronic"
-            isInCart= {true}
-            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-            />
-          <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
-            />
-          <Card 
-            title="Deneme Title"
-            description= "Deneme Desc"
-            currency= '£'
-            price= {20.50}
-            score= {5}
-            category="Elektronic"
-            isInCart= {false}
-            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-            />
-          <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
-            />
-          <Card 
-            title="Deneme Title"
-            description= "Deneme Desc"
-            currency= '£'
-            price= {20.50}
-            score= {5}
-            category="Elektronic"
-            isInCart= {true}
-            img="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-            />
+          {products.map((product) => (
             <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
+              key={product.id}
+              title={product.title}
+              description= {product.description}
+              currency= '€'
+              price= {product.price}
+              score= {product.rating.rate}
+              category={product.category}
+              isInCart= {false}
+              img={product.image}
             />
-            <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
-            />
-            <Card 
-            title='A Nice Dress'
-            description= 'This dress made by cotton and beautiful'
-            currency= '€'
-            price= {99.99}
-            score= {3.9}
-            category= 'Clothes'
-            isInCart= {false}
-            img= 'https://images.unsplash.com/photo-1618932260643-eee4a2f652a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=780&q=80'
-            />
+          ))}
         </div>
       </div>
     
