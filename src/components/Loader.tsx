e889df8853b4f3ce3c82538e1213edc3c6182d0b
KARIYER\saim.ankarali <akbeta08@gmail.com>
2021-10-30 10:12:54 +0300

    refactor: delete ball gif add spinner gif

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Ball from'../assets/ball.gif';
+import Spinner from'../assets/spinner.gif';
 
 
 function Loader() {
     return (
         <div className="loader-container fixed left-0 top-0 w-full h-full">
-            <img src={Ball} alt="" className="loader top-1/4 z-10 absolute"/>
+            <img src={Spinner} alt="" className="loader top-1/4 z-10 absolute"/>
             <p className="loader-text top-2/4 z-10 absolute">Loading...</p>
         </div>
     )
