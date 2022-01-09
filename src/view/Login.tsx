1a6f2ef2276272dd059b49d470ad8ab043499f76
KARIYER\saim.ankarali <akbeta08@gmail.com>
2022-01-09 23:11:40 +0300

    refactor: update login page

--- src/view/Login.tsx
+++ src/view/Login.tsx
@@ -3,23 +3,34 @@ import React from 'react'
 interface Props {}
 
 const Login = (props: Props) => {
-  return <>
-    <div>
-      <div className="container w h-screen	flex justify-center align-middle m-auto">
-        <div className="row flex flex-col justify-center w-1/4">
-          <h1 className="mb-4 flex justify-center">Log In</h1>
-          <div className="col-12 mt-4">
-            <label>Mail</label>
-            <input type="text" className='mt-2 border focus:outline-none flex justify-end w-full' />
+  return <div>
+    <div className="w-full h-screen flex items-center justify-center">
+        <form className="w-full md:w-1/3 bg-white rounded-lg">
+            <h2 className="text-3xl text-center text-gray-700 mb-4">Log In Form</h2>
+            <div className="px-12 pb-10">
+                <div className="w-full my-3">
+                    <div className="flex items-center">
+                        <i className='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user'></i>
+                        <input type='text' placeholder="Username or Mail"
+                            className="-mx-6 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
+                    </div>
+                </div>
+                <div className="w-full my-3">
+                    <div className="flex items-center">
+                        <i className='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
+                        <input type='password' placeholder="Password"
+                            className="-mx-6 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
+                    </div>
+                </div>
+                
+                <p>If you have not any account you should <a href="/signup" className="text-gray-500 text-lg"> Sign Up</a></p>
+                <a href="#" className="text-xs text-gray-500 float-right mb-4">Forgot Password?</a>
+                <button type="submit"
+                    className="w-full py-2 rounded-full bg-white text-black focus:outline-none border border-black hover:bg-black hover:text-white">Log In</button>
           </div>
-          <div className="col-12 mt-2">
-            <label>Password</label>
-            <input type="text" className='mt-2 border focus:outline-none flex justify-end w-full'/>
-          </div>
-          <button className="mt-4 p-1 border">Log In</button>
-        </div>
-      </div>
+        </form>
     </div>
-  </>
+    
+  </div>
 }
 export default Login
